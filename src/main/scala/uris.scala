ab1889dcfd03bbdb7399726fcb6c11f27dd7fa42
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-05-15 21:30:42 +0300

    feat: add some github api uri builders
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- src/main/scala/uris.scala
+++ src/main/scala/uris.scala
@@ -5,4 +5,11 @@ object uris {
 
   def uri(url: String): IO[Uri] = IO.fromEither(Uri.fromString(url))
 
+  def repos(org: String): String = s"https://api.github.com/orgs/$org/repos"
+
+  def repo(owner: String, repo: String): String = s"https://api.github.com/repos/$owner/$repo"
+
+  def commits(owner: String, repo: String, page: Int): String =
+    s"https://api.github.com/repos/$owner/$repo/commits?per_page=100&page=$page"
+
 }
